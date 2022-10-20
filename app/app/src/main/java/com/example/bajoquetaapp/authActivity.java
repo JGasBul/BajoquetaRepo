fc65737e550ea6c2f1d7ac670045fefa45d901c9
Pepe Gascó Bule <pepegascobule@gmail.com>
2022-10-20 21:13:54 +0200

    LoginPage

--- app/app/src/main/java/com/example/bajoquetaapp/authActivity.java
+++ app/app/src/main/java/com/example/bajoquetaapp/authActivity.java
@@ -1,41 +1,18 @@
 package com.example.bajoquetaapp;
-
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
-
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.identity.BeginSignInResult;
-import com.google.android.gms.auth.api.identity.Identity;
-import com.google.android.gms.auth.api.identity.SignInClient;
-import com.google.android.gms.auth.api.identity.SignInCredential;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.CommonStatusCodes;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class authActivity extends AppCompatActivity {
 
@@ -44,9 +21,10 @@ public class authActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_auth);
+        setContentView(R.layout.activity_auth);
         login();
     }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode,Intent data){
@@ -54,16 +32,34 @@ public class authActivity extends AppCompatActivity {
         if (requestCode == RC_SIGN_IN) {
             if (resultCode == RESULT_OK) {
                 login();
-                //finish(); //Quitar
             } else {
                 String s = "";
                 IdpResponse response = IdpResponse.fromResultIntent(data);
                 if (response == null) s = "Cancelado";
-                else switch (response.getError().getErrorCode()) {
-                    case ErrorCodes.NO_NETWORK: s="Sin conexión a Internet"; break;
-                    case ErrorCodes.PROVIDER_ERROR: s="Error en proveedor"; break;
-                    case ErrorCodes.DEVELOPER_ERROR: s="Error desarrollador"; break;
-                    default: s="Otros errores de autentificación";
+                else switch (Objects.requireNonNull(response.getError()).getErrorCode()) {
+                    case ErrorCodes.NO_NETWORK:
+                        s = "Sin conexión a Internet";
+                        break;
+                    case ErrorCodes.PROVIDER_ERROR:
+                        s = "Error en proveedor";
+                        break;
+                    case ErrorCodes.DEVELOPER_ERROR:
+                        s = "Error desarrollador";
+                        break;
+                    default:
+                        s = "Otros errores de autentificación";
+                    case ErrorCodes.ANONYMOUS_UPGRADE_MERGE_CONFLICT:
+                    case ErrorCodes.EMAIL_LINK_CROSS_DEVICE_LINKING_ERROR:
+                    case ErrorCodes.EMAIL_LINK_DIFFERENT_ANONYMOUS_USER_ERROR:
+                    case ErrorCodes.EMAIL_LINK_PROMPT_FOR_EMAIL_ERROR:
+                    case ErrorCodes.EMAIL_LINK_WRONG_DEVICE_ERROR:
+                    case ErrorCodes.EMAIL_MISMATCH_ERROR:
+                    case ErrorCodes.ERROR_GENERIC_IDP_RECOVERABLE_ERROR:
+                    case ErrorCodes.ERROR_USER_DISABLED:
+                    case ErrorCodes.INVALID_EMAIL_LINK_ERROR:
+                    case ErrorCodes.PLAY_SERVICES_UPDATE_CANCELLED:
+                    case ErrorCodes.UNKNOWN_ERROR:
+                        break;
                 }
                 Toast.makeText(this, s, Toast.LENGTH_LONG).show();
             }
@@ -74,8 +70,8 @@ public class authActivity extends AppCompatActivity {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         AuthMethodPickerLayout customLayout = new AuthMethodPickerLayout
                 .Builder(R.layout.activity_auth)
-                .setGoogleButtonId(R.id.bar)
-                .setEmailButtonId(R.id.foo)
+                .setGoogleButtonId(R.id.googleButton)
+                .setEmailButtonId(R.id.registerButton)
                 //.setTosAndPrivacyPolicyId(R.id.baz)
                 .build();
         if (user != null) {
--- app/app/src/main/java/com/example/bajoquetaapp/welcomeActivity.java
+++ app/app/src/main/java/com/example/bajoquetaapp/welcomeActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class welcomeActivity extends AppCompatActivity {
-private static final int SPLASH_TIME_OUT = 4000;
+private static final int SPLASH_TIME_OUT = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
