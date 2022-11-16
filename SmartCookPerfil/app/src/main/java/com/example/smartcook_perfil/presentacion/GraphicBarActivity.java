c6c198bbb577c3f7098b3c6f4954701e7050817d
Arnau <clavileno3000@gmail.com>
2022-11-16 18:04:23 +0100

    UserActivity

---
+++ SmartCookPerfil/app/src/main/java/com/example/smartcook_perfil/presentacion/GraphicBarActivity.java
@@ -0,0 +1,34 @@
+package com.example.smartcook_perfil.presentacion;
+
+import android.graphics.Color;
+
+import com.example.smartcook_perfil.R;
+import com.example.smartcook_perfil.data.GraphicBar;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class GraphicBarActivity {
+    //variables
+//    ArrayList barArrayList;
+//    public void onCreateBarChart(){
+//        GraphicBar graphicBar;
+//        GraphicBar barChart;
+//        graphicBar.getData(barArrayList);
+//
+//        BarDataSet barDataSet = new BarDataSet(barArrayList, "");
+//        BarData barData = new BarData(barDataSet);
+//        barChart.setData(barData);
+//        //color bar data set
+//        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+//        //text color
+//        barDataSet.setValueTextColor(Color.BLACK);
+//        //Setting text size
+//        barDataSet.setValueTextSize(16f);
+//        barChart.getDescription().setEnabled(true);
+//    }
+}
