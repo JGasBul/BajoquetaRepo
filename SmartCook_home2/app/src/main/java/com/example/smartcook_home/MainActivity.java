cf4aa1282af3cc807d4f4a65c9e3d9eb26e138e3
Zaida <zaidapastor2002@gmail.com>
2022-11-17 16:23:59 +0100

    Home page of SmartCook

---
+++ SmartCook_home2/app/src/main/java/com/example/smartcook_home/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.smartcook_home;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.smartcook_home.databinding.ActivityScrollingBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityScrollingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityScrollingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void rodar(View view){
+        ImageView spaceshipImage = findViewById(R.id.cara);
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this, R.anim.hyperspace_jump);
+        spaceshipImage.startAnimation(hyperspaceJumpAnimation);
+
+    }
+
+}
