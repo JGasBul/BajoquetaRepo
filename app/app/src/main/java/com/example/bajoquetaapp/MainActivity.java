7360a896478c47b0a71b87893c62992f49ee309d
Pepe Gascó Bule <pepegascobule@gmail.com>
2022-11-17 21:34:34 +0100

    AddHome Page to Fragments

--- app/app/src/main/java/com/example/bajoquetaapp/MainActivity.java
+++ app/app/src/main/java/com/example/bajoquetaapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.bajoquetaapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,11 +32,18 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_user, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_user, R.id.navigation_recipes)
                 .build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+    }
+
+    public void rodar(View view){
+        ImageView spaceshipImage = findViewById(R.id.cara);
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this, R.anim.hyperspace_jump);
+        spaceshipImage.startAnimation(hyperspaceJumpAnimation);
 
     }
 
